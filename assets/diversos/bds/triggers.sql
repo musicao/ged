9788051ec2a4e919df49eb8b7b58bc96e3090cba
Israel Musicao <israeleduardo@gmail.com>
2016-02-13 23:33:11 -0200

    ajustes bds .sql

--- assets/diversos/bds/triggers.sql
+++ assets/diversos/bds/triggers.sql
@@ -10,3 +10,52 @@ DELIMITER //
 				END IF; 
 		END // 
 DELIMITER ;
+
+
+DELIMITER //
+CREATE TRIGGER `TRG_EntradaProduto_AI` AFTER INSERT ON `entrada_produto`
+FOR EACH ROW
+BEGIN
+      CALL SP_AtualizaEstoque (new.id_produto, new.qtde, new.valor_unitario);
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER `TRG_EntradaProduto_AU` AFTER UPDATE ON `entrada_produto`
+FOR EACH ROW
+BEGIN
+      CALL SP_AtualizaEstoque (new.id_produto, new.qtde - old.qtde, new.valor_unitario);
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER `TRG_EntradaProduto_AD` AFTER DELETE ON `entrada_produto`
+FOR EACH ROW
+BEGIN
+      CALL SP_AtualizaEstoque (old.id_produto, old.qtde * -1, old.valor_unitario);
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER `TRG_SaidaProduto_AI` AFTER INSERT ON `saida_produto`
+FOR EACH ROW
+BEGIN
+      CALL SP_AtualizaEstoque (new.id_produto, new.qtde * -1, new.valor_unitario);
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER `TRG_SaidaProduto_AU` AFTER UPDATE ON `saida_produto`
+FOR EACH ROW
+BEGIN
+      CALL SP_AtualizaEstoque (new.id_produto, old.qtde - new.qtde, new.valor_unitario);
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER `TRG_SaidaProduto_AD` AFTER DELETE ON `saida_produto`
+FOR EACH ROW
+BEGIN
+      CALL SP_AtualizaEstoque (old.id_produto, old.qtde, old.valor_unitario);
+END //
+DELIMITER ;
